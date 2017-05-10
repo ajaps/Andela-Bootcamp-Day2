c0685e8389b5f654e6d8b86330758b650f515625
AjapsFranklin <framky007@yahoo.com>
2017-05-10 16:39:38 +0100

    Adds implementation file for Word Frequency

---
+++ word-frequency/app/index.js
@@ -0,0 +1,29 @@
+function words(stringAugument){
+
+  //remove escape character and double spaces
+  stringAugument  =stringAugument.replace('\n',' ');
+  stringAugument  =stringAugument.replace('\t',' ');
+  stringAugument  =stringAugument.replace('  ',' ');
+
+  //split the sentence 
+  let wordArray = stringAugument.split(' ');
+  let wordFrequecy ={}
+
+  //loop through the array and asign numbers against the words based on their frequecy 
+  for(eachWord in wordArray){
+    theWord = wordArray[eachWord];
+    if(theWord in wordFrequecy){
+      wordFrequecy[theWord] = wordFrequecy[theWord] + 1;
+    }
+    else{
+        wordFrequecy[theWord] = 1;
+    }
+  }
+  return wordFrequecy;
+}
+
+
+
+module.exports = {
+  words : words
+}
